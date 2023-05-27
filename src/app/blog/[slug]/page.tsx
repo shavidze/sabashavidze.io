4819622c3521075a55feef247245f6e9d506b09e
Saba Shavidze <sabashavidze@gmail.com>
2023-05-27 21:31:15 +0400

    styled rendered mdx / added rehype plugin for highlight color

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -70,7 +70,7 @@ const BlogPost: FC<Props> = ({ params }) => {
           {JSON.stringify(post.structuredData)}
         </script>
       )}
-      <h1 className="font-bold text-3xl font-serif max-w-[650px]">
+      <h1 className="font-bold text-3xl font-serif max-w-[850px]">
         <Balancer>{post.title}</Balancer>
       </h1>
       <div className="grid grid-cols-[auto_1fr_auto] items-center mt-4 mb-8 font-mono text-sm max-w-[650px]">
