39ffc2c7acd6ad05e217a297dffcf999e1acc4f9
Saba Shavidze <sabashavidze@gmail.com>
2023-05-29 17:35:36 +0400

    add robots.ts and sitemap for it

---
+++ src/app/sitemap.ts
@@ -0,0 +1,19 @@
+import { allPosts } from 'contentlayer/generated';
+import { MetadataRoute } from 'next';
+
+export default function sitemap(): MetadataRoute.Sitemap {
+  const blogs = allPosts.map((post) => {
+    return {
+      url: `https://sabashavidze.io/blog/${post.slug}`,
+      lastModified: post.publishedAt,
+    };
+  });
+
+  const routes = ['', '/about', '/blog'].map((route) => {
+    return {
+      url: `https://sabashavidze.io/${route}`,
+      lastModified: new Date().toISOString().split('T')[0],
+    };
+  });
+  return [...blogs, ...routes];
+}
