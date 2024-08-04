5d902ba3dd9b27efc29fa95a0c25a4999f86c95e
Saba Shavidze <sabashavidze@gmail.com>
2024-08-04 14:30:49 +0400

    ref: update about sec

--- src/lib/about-me.tsx
+++ src/lib/about-me.tsx
@@ -14,18 +14,9 @@ export const name = 'Saba Shavidze';
 export const avatar = me;
 
 export const About = () => {
-  return (
-    <>
-      I&rsquo;m Saba.I&rsquo;m a full-stack Software Engineer with a deep
-      passion for web development. While my expertise lies in traditional
-      programming, my journey expands into exciting frontiers. I embrace
-      cutting-edge technologies, exploring the potential of blockchain,
-      functional programming paradigms, and distributed systems. I thrive on
-      staying updated with the latest trends in the dynamic realm of software
-      engineering, continuously evolving to deliver innovative solutions
-    </>
-  );
+  return <>Hey, I&rsquo;m Saba. I like engineering.</>;
 };
+
 export const Bio: FC<{}> = () => {
   const container = {
     hidden: { opacity: 0 },
@@ -54,46 +45,29 @@ export const Bio: FC<{}> = () => {
         className="prose prose-neutral dark:prose-invert text-neutral-800 dark:text-neutral-200"
       >
         <motion.p variants={item}>
-          As a <b>Fullstack JavaScript Developer</b> with experience in
-          <b> Angular, React, Next.js, Sveltekit, Node.js, and Nest.js</b>, I
-          have a strong technical background, analytical skills, and a
-          problem-solving mindset. In my previous roles, I actively listened to
-          customer requests, implemented better user experience, and assisted
-          other senior developers to meet pressing deadlines. I have experience
-          working with both <b>SQL and NoSQL databases</b>, and I am proficient
-          in writing queries and managing database operations. Additionally, I
-          am well-versed in utilizing containerization with <b>Docker</b> to
-          create portable and scalable applications.
+          As a <b>Engineer</b> with expertise in <b>C#, .NET, and JavaScript</b>
+          , I bring a strong technical background, analytical skills, and a
+          problem-solving mindset to my work. In my current role within the
+          banking industry, I handle extensive database operations, primarily
+          focusing on <b>SQL</b>. I actively listen to customer requests,
+          implement enhancements for better user experiences, and collaborate
+          with senior developers to meet critical deadlines. My experience
+          includes managing both <b>SQL and NoSQL databases</b>, proficiently
+          writing queries, and optimizing database performance. Additionally, I
+          utilize containerization with <b>Docker</b> to create scalable and
+          portable applications.
         </motion.p>
         <hr />
         <motion.p variants={item}>
-          My expertise extends to various UI libraries, including{' '}
-          <b>
-            Material, Clarity, Kendo UI, Mantine, Skeleton UI, and Tailwind{' '}
-          </b>
-          enabling me to develop visually appealing and intuitive user
-          interfaces. I have also worked with video players, both by utilizing
-          existing libraries and by developing them from scratch, which has
-          given me a deep understanding of multimedia integration and playback
-          in web applications.
-        </motion.p>
-        <hr />
-        <motion.p variants={item}>
-          Furthermore, my passion for <b>algorithms and data structures </b>
-          enhances my ability to develop efficient and performant web
-          applications. I continuously strive to optimize code and explore
-          innovative approaches to problem-solving. With my comprehensive skill
-          set and experience in web development, along with my proficiency in
-          <b> Node.js, Nest.js, SQL, NoSQL databases, and Docker</b>, I am
-          well-equipped to tackle diverse challenges and contribute to the
-          success of web application development projects.
-        </motion.p>
-        <motion.p variants={item} className="mb-8">
-          Moreover, I am familiar with <b>Agile SCRUM methodology</b> and have
-          actively participated in agile development processes, including sprint
-          planning, daily stand-ups, and retrospectives. I thrive in
-          collaborative environments, where I can contribute to cross-functional
-          teams and engage in effective communication to ensure project success.
+          I have complex expertise with <b>AWS</b> and deployment using{' '}
+          <b>Terraform</b> and <b>GitLab/GitHub Actions</b>. This allows me to
+          manage cloud infrastructure and automate deployment processes
+          effectively. Furthermore, I am well-versed in{' '}
+          <b>Agile SCRUM methodology</b> and actively participate in agile
+          development processes, including sprint planning, daily stand-ups, and
+          retrospectives. I thrive in collaborative environments, contributing
+          to cross-functional teams and engaging in effective communication to
+          ensure project success.
         </motion.p>
         <motion.div
           variants={item}
